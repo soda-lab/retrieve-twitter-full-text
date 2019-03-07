0444f4181866ab7a6704d48e15c00283becf2193
Chris <chrislishuanghong@gmail.com>
2019-03-07 14:16:17 +1100

    delete old_version of retrieving full tweets

--- mongodb-full-text.sh
+++
@@ -1,3 +0,0 @@
-\export PATH="/mnt/applications/anaconda3/bin:$PATH"
-sh /mnt/applications/anaconda3/bin/activate
-jupyter nbconvert --ExecutePreprocessor.timeout=0  --execute mongodb-full-text.ipynb
