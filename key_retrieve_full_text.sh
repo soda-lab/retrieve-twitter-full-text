98d568fb0f116f5902b1bce4b21a025a37d46e08
Chris <chrislishuanghong@gmail.com>
2019-03-07 15:25:18 +1100

    update key_retrieve_text.sh

--- key_retrieve_full_text.sh
+++ key_retrieve_full_text.sh
@@ -1,3 +1,4 @@
 \export PATH="/mnt/applications/anaconda3/bin:$PATH"
 sh /mnt/applications/anaconda3/bin/activate
-jupyter nbconvert --ExecutePreprocessor.timeout=0  --execute first_key_retrieve_full_text.ipynb
+jupyter nbconvert --to script key_retrieve_full_text.ipynb
+python3 key_retrieve_full_text.py
